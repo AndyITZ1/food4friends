06e88dd3d817341f5aff2f4ada4a32fe18a64b4e
Andy Tran <ltran20sk@gmail.com>
2025-04-17 19:34:51 -0400

    Updated code to migrate auth to navbar.

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,37 +1,7 @@
-
-import { signIn, signOut, auth } from "@/auth"
-import { constants } from "node:fs/promises"
-
-export default async function SignIn() {
-    const session = await auth();
-    console.log(session);
-    const user = session?.user;
-
-    return user ? (
-        <>
-            <h1 className="text-2xl font-bold">Welcome {user.name}</h1>
-            <form
-                action={async () => {
-                    "use server"
-                    await signOut()
-                }}>
-                <button className="bg-red-500 text-white p-2 rounded" type="submit">Sign Out</button>
-            </form>
+export default function Profile() {
+    return (
+        <>  
+            <div>Profile Page</div>
         </>
-    ) :
-        (
-            <>
-                <h1 className="text-2xl font-bold">Please sign in to access your profile</h1>
-                <form
-                    action={async () => {
-                        "use server"
-                        await signIn("google");
-                    }}>
-                    <button className="bg-blue-500 text-white p-2 rounded" type="submit">Sign In</button>
-                </form>
-            </>
-        )
-
-
-
+    )
 } 
